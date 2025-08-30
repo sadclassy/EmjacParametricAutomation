59783aef5dae2e5fa165d51c66060cf761039b13
sadclassy <christiand@emjacindustries.com>
2025-08-29 21:00:41 -0400

    Update guicomponent.h

--- guicomponent.h
+++ guicomponent.h
@@ -8,6 +8,7 @@
 
 
 
+
 typedef struct {
     SymbolTable* st;
     char* param_name;
@@ -33,27 +34,67 @@ typedef struct {
 } UserSelectData;
 
 typedef struct {
+    SymbolTable* st;
+    char reference[128];  // Sufficient size for reference strings
+} UpdateData;
+
+typedef struct {
+    SymbolTable* st;
+    char* filter;  // Existing
+    UserSelectOptionalNode* node;  // Existing
+    CreoReferenceType* allowed_types;  // New: Array of allowed enums (e.g., CREO_SURFACE, CREO_CURVE)
+    size_t type_count;  // New: Number of allowed types
+    char draw_area_id[128];
+    char button_id[128];
+} UserSelectOptionalData;
+
+typedef struct {
+    SymbolTable* st;
+    char* filter;  // Existing
+    UserSelectMultipleNode* node;  // Existing
+    CreoReferenceType* allowed_types;  // New: Array of allowed enums (e.g., CREO_SURFACE, CREO_CURVE)
+    size_t type_count;  // New: Number of allowed types
+    char draw_area_id[128];
+    char button_id[128];
+} UserSelectMultipleData;
+
+typedef struct {
+    SymbolTable* st;
+    char* filter;  // Existing
+    UserSelectMultipleOptionalNode* node;  // Existing
+    CreoReferenceType* allowed_types;  // New: Array of allowed enums (e.g., CREO_SURFACE, CREO_CURVE)
+    size_t type_count;  // New: Number of allowed types
+    char draw_area_id[128];
+    char button_id[128];
+} UserSelectMultipleOptionalData;
+
+typedef struct {
     SymbolTable* st;     // Pointer to the symbol table for variable updates
     char* parameter;     // Parameter name for the variable to update
     RadioButtonParamNode node;
 } RadioSelectData;
 
 
-
 ProError draw_global_picture(char* dialog, char* component, ProAppData app_data);
 ProError draw_sub_pictures(char* dialog, char* component, ProAppData app_data);
+ProError OnPictureShowParam(char* dialog, char* draw_area_name, ShowParamNode* node, SymbolTable* st);
 ProError addShowParam(char* dialog_name, char* parent_layout_name, ShowParamNode* node, int* current_row, int column, SymbolTable* st);
+ProError OnPictureCheckboxParam(char* dialog, char* draw_area_name, CheckboxParamNode* node, SymbolTable* st);
 ProError addCheckboxParam(char* dialog_name, char* parent_layout_name, CheckboxParamNode* cbNode, int* current_row, int column, SymbolTable* st);
 ProError addUserInputParam(char* dialog_name, char* parent_layout_name, UserInputParamNode* node, int* current_row, int column, SymbolTable* st);
 ProError addRadioButtonParam(char* dialog_name, char* parent_layout_name, RadioButtonParamNode* node, int* current_row, int column, SymbolTable* st);
 ProError addUserSelect(char* dialog_name, char* parent_layout_name, UserSelectNode* node, int* current_row, int column, SymbolTable* st);
+ProError addUserSelectOptional(char* dialog_name, char* parent_layout_name, UserSelectOptionalNode* node, int* current_row, int column, SymbolTable* st);
+ProError addUserSelectMultipleOptional(char* dialog_name, char* parent_layout_name, UserSelectMultipleOptionalNode* node, int* current_row, int column, SymbolTable* st);
+ProError addUserSelectMultiple(char* dialog_name, char* parent_layout_name, UserSelectMultipleNode* node, int* current_row, int column, SymbolTable* st);
 ProError addpicture(char* dialog, char* component, ProAppData app_data);
 ProError MyPostManageCallback(char* dialog, char* component, ProAppData app_data);
 ProError PushButtonAction(char* dialog, char* component, ProAppData app_data);
 ProError CloseCallback(char* dialog_name, char* component, ProAppData app_data);
 ProError validate_ok_button(char* dialog, SymbolTable* st);
 wchar_t* variable_value_to_wstring(const Variable* var);
-void toggle_user_selects_in_block(Block* blk, SymbolTable* st, const char* dialog, ProBoolean enabled);
+void set_bool_in_map(HashTable* map, const char* key, int on);
+ProError set_user_select_enabled(char* dialog, SymbolTable* st, const char* reference, ProBoolean enabled, ProBoolean required);
 
 
 
