54f8ca05a0942526340f924fc16769f38a77c92e
sadclassy <christiand@emjacindustries.com>
2025-08-29 21:03:27 -0400

    Update ScriptExecutor.h

--- ScriptExecutor.h
+++ ScriptExecutor.h
@@ -5,6 +5,8 @@
 #include "syntaxanalysis.h"
 #include "utility.h"
 
+#define MAX_SUBTABLE_LEVELS 20
+
 
 typedef void(*ExecuteFunction)(void* data, SymbolTable* st, BlockList* block_list);
 
@@ -17,49 +19,41 @@ typedef struct
     char* confirmation_layout_name;
     int global_row_counter;
     Block* gui_block;  // Added for access during updates
+    Block* tab_block;
     SymbolTable* st;
+    int  root_table_built;           /* 0/1 */
+    char root_identifier[128];       /* optional: which table to build as root */
+    bool needs_show_param;
+    bool needs_checkbox;
+    bool needs_user_input;
+    bool needs_radiobutton;
+    bool needs_user_select;
+    bool needs_picture;
+    int num_param_sections;
 
-    struct
-    {
-        char** refs;
-        size_t count;
-        size_t capacity;
-    } required_user_select;
 
-    struct
-    {
-        int initialized;
-        int row;
-        char name[100];
-    } show_param_layout;
-    struct
-    {
-        int initialized;
-        int row;
-        char name[100];
-    } user_input_layout;
-    struct
-    {
-        int initialized;
-        int row;
-        char name[100];
-    } radiobutton_layout;
-    struct
-    {
-        int initialized;
-        int row;
-        char name[100];
-    } checkbox_layout;
-    struct
-    {
-        int initialized;
-        char name[100];
-        int s_us_grid_initialized;
-        int row;
-    } user_select_layout;
 
+    struct { char** refs; size_t count; size_t capacity; } required_user_select;
+    struct { int initialized; int row; char name[100]; } show_param_layout;
+    struct { int initialized; int row; char name[100]; } user_input_layout;
+    struct { int initialized; int row; char name[100]; } radiobutton_layout;
+    struct { int initialized; int row; char name[100]; } checkbox_layout;
+    struct { int initialized; char name[100]; int s_us_grid_initialized; int s_us_gridO_initialize; int s_us_grid1_initialized; int s_us_gridM_initialized; int row; } user_select_layout;
+    struct { int initialized; char name[100]; int column; int row; } indvidual_table;
 } DialogState;
 
+
+/* -------------------- NEW: unified IF execution context -------------------- */
+typedef struct ExecContext {
+    SymbolTable* st;
+    BlockList* block_list;  /* may be NULL */
+    DialogState* ui;          /* NULL for non-GUI */
+} ExecContext;
+
+/* Unified walkers */
+ProError exec_command_in_context(CommandNode* node, ExecContext* ctx);
+ProError execute_if_ctx(IfNode* node, ExecContext* ctx);
+/* -------------------------------------------------------------------------- */
 
 ProError execute_command(CommandNode* node, SymbolTable* st, BlockList* block_list);
 ProError execute_declare_variable(DeclareVariableNode* node, SymbolTable* st);
